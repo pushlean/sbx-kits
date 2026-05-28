bf98695adddfd33c5d8609ea824af4fd03e245ee
Paul Shen <paul@pushlean.com>
2026-05-28 14:02:20 -0700

    [nvim] inline config files into spec.yaml

--- nvim/files/home/.config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd 'source ~/.vimrc-keys'
-
--- Save and exit with Ctrl+s
-vim.api.nvim_set_keymap("i", "<C-s>", "<esc>:wq!<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-s>", ":wq!<cr>", { noremap = true })
